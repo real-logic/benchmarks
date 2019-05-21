b18a3dc9a06218572c3e715347c3b0a120dad61e
Todd L. Montgomery <tmont@nard.net>
2019-05-20 17:03:42 -0700

    [C++]: add time benchmark

---
+++ src/main/cpp/TimeBenchmark.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2014-2017 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <chrono>
+
+#include "benchmark/benchmark.h"
+
+using namespace std;
+
+static void BM_steady_clock(benchmark::State &state)
+{
+    while (state.KeepRunning())
+    {
+        benchmark::DoNotOptimize(chrono::steady_clock::now());
+    }
+}
+
+BENCHMARK(BM_steady_clock);
+
+static void BM_high_resolution_clock(benchmark::State &state)
+{
+    while (state.KeepRunning())
+    {
+        benchmark::DoNotOptimize(chrono::high_resolution_clock::now());
+    }
+}
+
+BENCHMARK(BM_high_resolution_clock);
+
+static void BM_system_clock(benchmark::State &state)
+{
+    while (state.KeepRunning())
+    {
+        benchmark::DoNotOptimize(chrono::system_clock::now());
+    }
+}
+
+BENCHMARK(BM_system_clock);
+
+BENCHMARK_MAIN();
