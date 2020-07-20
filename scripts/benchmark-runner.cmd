2ba0f51b74d464d8672fc72a12bfd3c54d68c2a5
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2020-07-20 15:54:51 +0300

    [Java] Add Windows version of the benchmark-runner script.

---
+++ scripts/benchmark-runner.cmd
@@ -0,0 +1,114 @@
+::
+:: Copyright 2015-2020 Real Logic Limited.
+::
+:: Licensed under the Apache License, Version 2.0 (the "License");
+:: you may not use this file except in compliance with the License.
+:: You may obtain a copy of the License at
+::
+:: https://www.apache.org/licenses/LICENSE-2.0
+::
+:: Unless required by applicable law or agreed to in writing, software
+:: distributed under the License is distributed on an "AS IS" BASIS,
+:: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+:: See the License for the specific language governing permissions and
+:: limitations under the License.
+::
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+
+rem
+rem Example: Invoking `benchmark-runner` from the `scripts` directory.
+rem
+rem $ benchmark-runner.cmd --output-file "echo-test" --messages "1000, 5000" --burst-size "1, 10" --message-length "16, 200, 1400" "aeron\echo-client"
+rem
+
+set RUNS=5
+set ITERATIONS=10
+set NUMBER_OF_MESSAGES=10000
+set BURST_SIZE=1
+set MESSAGE_LENGTH=16
+
+:loop
+if not "%1"=="" (
+  set "FLAG="
+  if "%1"=="--output-file" set FLAG=1
+  if "%1"=="-o" set FLAG=1
+  if defined FLAG (
+      set "OUTPUT_FILE_NAME=%2";
+  )
+
+  set "FLAG="
+  if "%1"=="--messages" set FLAG=1
+  if "%1"=="-m" set FLAG=1
+  if defined FLAG (
+      set "NUMBER_OF_MESSAGES=%2";
+  )
+
+  set "FLAG="
+  if "%1"=="--burst-size" set FLAG=1
+  if "%1"=="-b" set FLAG=1
+  if defined FLAG (
+      set "BURST_SIZE=%2";
+  )
+
+  set "FLAG="
+  if "%1"=="--message-length" set FLAG=1
+  if "%1"=="-l" set FLAG=1
+  if defined FLAG (
+      set "MESSAGE_LENGTH=%2";
+  )
+
+  set "FLAG="
+  if "%1"=="--iterations" set FLAG=1
+  if "%1"=="-i" set FLAG=1
+  if defined FLAG (
+      set "ITERATIONS=%2";
+  )
+
+  set "FLAG="
+  if "%1"=="--runs" set FLAG=1
+  if "%1"=="-r" set FLAG=1
+  if defined FLAG (
+      set "RUNS=%2";
+  )
+
+  set "FLAG="
+  if "%1"=="--help" set FLAG=1
+  if "%1"=="-h" set FLAG=1
+  if defined FLAG (
+      echo "%0 (-o|--output-file) ^"\$output-file-name-prefix\" [(-m|--messages) ^"\$number-of-messages-csv\"] [(-b|--burst-size) ^"\$burst-size-csv\"] [(-l|--message-length) ^"\$message-length-in-bytes-csv\"] [(-i|--iterations) ^$iterations] [(-r|--runs) ^$runs] ^"\$command ^$cmdArg1 ...\""
+      exit /b
+  )
+
+  set "COMMAND=%1"
+  shift
+  goto :loop
+)
+
+if [%OUTPUT_FILE_NAME%] == [] (
+  echo 'Flag -o/--output-file is required'
+  exit -1
+)
+
+set "EXISTING_JVM_OPTS=%JVM_OPTS%"
+
+for %%messages in (%NUMBER_OF_MESSAGES%) do (
+  for %%burst in (%BURST_SIZE%) do (
+    for %%length in (%MESSAGE_LENGTH%) do (
+      for /L %%r in (1,1,%RUNS%) do (
+        echo -e 'Run #'%%r' ...\r\n'
+
+        set "JVM_OPTS=%EXISTING_JVM_OPTS ^
+        -Duk.co.real_logic.benchmarks.remote.outputFileNamePrefix=%OUTPUT_FILE_NAME% ^
+        -Duk.co.real_logic.benchmarks.remote.iterations=%ITERATIONS% ^
+        -Duk.co.real_logic.benchmarks.remote.messages=%%messages ^
+        -Duk.co.real_logic.benchmarks.remote.batchSize=%%burst ^
+        -Duk.co.real_logic.benchmarks.remote.messageLength=%%length"
+         
+         %COMMAND%
+      )
+    )
+  )
+)
+
+set "JVM_OPTS=%EXISTING_JVM_OPTS%"
