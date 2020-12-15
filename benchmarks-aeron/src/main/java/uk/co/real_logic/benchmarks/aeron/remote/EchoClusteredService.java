ce8004901fe1a3354b473f2c4e1b44740a6a56c5
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2020-12-15 12:04:47 +0100

    [Java] Add echo cluster benchmark.

---
+++ benchmarks-aeron/src/main/java/uk/co/real_logic/benchmarks/aeron/remote/EchoClusteredService.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2015-2020 Real Logic Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.real_logic.benchmarks.aeron.remote;
+
+import io.aeron.ExclusivePublication;
+import io.aeron.Image;
+import io.aeron.cluster.client.AeronCluster;
+import io.aeron.cluster.codecs.CloseReason;
+import io.aeron.cluster.service.ClientSession;
+import io.aeron.cluster.service.Cluster;
+import io.aeron.cluster.service.ClusteredService;
+import io.aeron.logbuffer.BufferClaim;
+import io.aeron.logbuffer.Header;
+import org.agrona.DirectBuffer;
+import org.agrona.MutableDirectBuffer;
+import org.agrona.concurrent.IdleStrategy;
+
+import static uk.co.real_logic.benchmarks.aeron.remote.AeronUtil.checkPublicationResult;
+
+public final class EchoClusteredService implements ClusteredService
+{
+    private final BufferClaim bufferClaim = new BufferClaim();
+    private IdleStrategy idleStrategy;
+
+    public void onStart(final Cluster cluster, final Image snapshotImage)
+    {
+        idleStrategy = cluster.idleStrategy();
+    }
+
+    public void onSessionOpen(final ClientSession session, final long timestamp)
+    {
+    }
+
+    public void onSessionClose(final ClientSession session, final long timestamp, final CloseReason closeReason)
+    {
+    }
+
+    public void onSessionMessage(
+        final ClientSession session,
+        final long timestamp,
+        final DirectBuffer buffer,
+        final int offset,
+        final int length,
+        final Header header)
+    {
+        final IdleStrategy idleStrategy = this.idleStrategy;
+        final BufferClaim bufferClaim = this.bufferClaim;
+
+        idleStrategy.reset();
+        long result;
+        while ((result = session.tryClaim(length, bufferClaim)) <= 0)
+        {
+            checkPublicationResult(result);
+            idleStrategy.idle();
+        }
+
+        final MutableDirectBuffer dstBuffer = bufferClaim.buffer();
+        final int msgOffset = bufferClaim.offset() + AeronCluster.SESSION_HEADER_LENGTH;
+
+        dstBuffer.putBytes(msgOffset, buffer, offset, length);
+
+        bufferClaim.flags(header.flags()).commit();
+    }
+
+    public void onTimerEvent(final long correlationId, final long timestamp)
+    {
+    }
+
+    public void onTakeSnapshot(final ExclusivePublication snapshotPublication)
+    {
+    }
+
+    public void onRoleChange(final Cluster.Role newRole)
+    {
+    }
+
+    public void onTerminate(final Cluster cluster)
+    {
+    }
+
+    public static void main(final String[] args)
+    {
+    }
+}
---
+++ benchmarks-aeron/src/test/java/uk/co/real_logic/benchmarks/aeron/remote/ClusterTest.java
@@ -0,0 +1,235 @@
+/*
+ * Copyright 2015-2020 Real Logic Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.real_logic.benchmarks.aeron.remote;
+
+import io.aeron.archive.Archive;
+import io.aeron.archive.client.AeronArchive;
+import io.aeron.cluster.ConsensusModule;
+import io.aeron.cluster.client.AeronCluster;
+import io.aeron.cluster.service.ClusteredServiceContainer;
+import org.agrona.ErrorHandler;
+import org.agrona.LangUtil;
+import org.agrona.collections.LongArrayList;
+import org.agrona.concurrent.NoOpLock;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.io.TempDir;
+import uk.co.real_logic.benchmarks.remote.Configuration;
+import uk.co.real_logic.benchmarks.remote.MessageTransceiver;
+
+import java.nio.file.Path;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static io.aeron.CommonContext.AERON_DIR_PROP_NAME;
+import static io.aeron.driver.Configuration.DIR_DELETE_ON_SHUTDOWN_PROP_NAME;
+import static io.aeron.driver.Configuration.DIR_DELETE_ON_START_PROP_NAME;
+import static java.lang.System.clearProperty;
+import static java.lang.System.setProperty;
+import static org.agrona.CloseHelper.closeAll;
+import static org.agrona.LangUtil.rethrowUnchecked;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static uk.co.real_logic.benchmarks.aeron.remote.AeronUtil.EMBEDDED_MEDIA_DRIVER_PROP_NAME;
+import static uk.co.real_logic.benchmarks.aeron.remote.AeronUtil.launchArchivingMediaDriver;
+
+class ClusterTest
+{
+    @BeforeEach
+    void before()
+    {
+        setProperty(EMBEDDED_MEDIA_DRIVER_PROP_NAME, "true");
+        setProperty(AeronArchive.Configuration.RECORDING_EVENTS_ENABLED_PROP_NAME, "false");
+        setProperty(DIR_DELETE_ON_START_PROP_NAME, "true");
+        setProperty(DIR_DELETE_ON_SHUTDOWN_PROP_NAME, "true");
+        setProperty(Archive.Configuration.ARCHIVE_DIR_DELETE_ON_START_PROP_NAME, "true");
+    }
+
+    @AfterEach
+    void after()
+    {
+        clearProperty(EMBEDDED_MEDIA_DRIVER_PROP_NAME);
+        clearProperty(DIR_DELETE_ON_START_PROP_NAME);
+        clearProperty(DIR_DELETE_ON_SHUTDOWN_PROP_NAME);
+        clearProperty(Archive.Configuration.ARCHIVE_DIR_DELETE_ON_START_PROP_NAME);
+        clearProperty(AERON_DIR_PROP_NAME);
+        clearProperty(Archive.Configuration.ARCHIVE_DIR_PROP_NAME);
+        clearProperty(AeronArchive.Configuration.RECORDING_EVENTS_ENABLED_PROP_NAME);
+        clearProperty(AeronArchive.Configuration.CONTROL_CHANNEL_PROP_NAME);
+        clearProperty(AeronArchive.Configuration.LOCAL_CONTROL_CHANNEL_PROP_NAME);
+    }
+
+    @Timeout(30)
+    @Test
+    void messageLength32bytes(final @TempDir Path tempDir) throws Exception
+    {
+        test(10_000, 32, 10, tempDir);
+    }
+
+    @Timeout(30)
+    @Test
+    void messageLength192bytes(final @TempDir Path tempDir) throws Exception
+    {
+        test(1000, 192, 5, tempDir);
+    }
+
+    @Timeout(30)
+    @Test
+    void messageLength1344bytes(final @TempDir Path tempDir) throws Exception
+    {
+        test(100, 1344, 1, tempDir);
+    }
+
+    @SuppressWarnings("MethodLength")
+    protected final void test(
+        final int messages,
+        final int messageLength,
+        final int burstSize,
+        final Path tempDir) throws Exception
+    {
+        final String aeronDirectoryName = tempDir.resolve("driver").toString();
+        setProperty(AERON_DIR_PROP_NAME, aeronDirectoryName);
+        setProperty(Archive.Configuration.ARCHIVE_DIR_PROP_NAME, tempDir.resolve("archive").toString());
+        setProperty(AeronArchive.Configuration.CONTROL_CHANNEL_PROP_NAME,
+            "aeron:udp?endpoint=localhost:8010|term-length=64k");
+        setProperty(AeronArchive.Configuration.LOCAL_CONTROL_CHANNEL_PROP_NAME, "aeron:ipc?term-length=64k");
+
+        final Configuration configuration = new Configuration.Builder()
+            .numberOfMessages(messages)
+            .messageLength(messageLength)
+            .messageTransceiverClass(EchoClusterMessageTransceiver.class)
+            .outputDirectory(tempDir)
+            .outputFileNamePrefix("aeron")
+            .build();
+
+        final AtomicReference<Throwable> error = new AtomicReference<>();
+        final LongArrayList receivedTimestamps = new LongArrayList(messages, Long.MIN_VALUE);
+        final LongArrayList sentTimestamps = new LongArrayList(messages, Long.MIN_VALUE);
+
+        final ArchivingMediaDriver driver = launchArchivingMediaDriver();
+
+        final AeronArchive.Context aeronArchiveContext = new AeronArchive.Context()
+            .lock(NoOpLock.INSTANCE)
+            .controlRequestChannel(AeronArchive.Configuration.controlChannel())
+            .controlRequestStreamId(AeronArchive.Configuration.controlStreamId())
+            .controlResponseChannel("aeron:udp?endpoint=localhost:0|term-length=64k")
+            .aeronDirectoryName(aeronDirectoryName);
+
+        final String clusterDirectoryName = tempDir.resolve("consensus-module").toString();
+        final ConsensusModule consensusModule = ConsensusModule.launch(new ConsensusModule.Context()
+            .clusterMemberId(0)
+            .clusterMembers("0,localhost:20000,localhost:20001,localhost:20002,localhost:20003,localhost:8010")
+            .ingressChannel("aeron:udp?term-length=64k")
+            .logChannel("aeron:udp?term-length=64k|control-mode=manual|control=localhost:20002")
+            .errorHandler(errorHandler("consensus-module"))
+            .archiveContext(aeronArchiveContext.clone())
+            .aeronDirectoryName(aeronDirectoryName)
+            .clusterDirectoryName(clusterDirectoryName));
+        final ClusteredServiceContainer clusteredServiceContainer = ClusteredServiceContainer.launch(
+            new ClusteredServiceContainer.Context()
+            .clusteredService(new EchoClusteredService())
+            .errorHandler(errorHandler("service-container"))
+            .archiveContext(aeronArchiveContext.clone())
+            .aeronDirectoryName(aeronDirectoryName)
+            .clusterDirectoryName(clusterDirectoryName));
+        final AtomicBoolean running = new AtomicBoolean(true);
+        try
+        {
+            final MessageTransceiver messageTransceiver = new EchoClusterMessageTransceiver(
+                null,
+                new AeronCluster.Context()
+                .ingressChannel("aeron:udp?term-length=64k")
+                .ingressEndpoints("0=localhost:20000")
+                .egressChannel("aeron:udp?endpoint=localhost:0|term-length=64k"),
+                (timestamp, checksum) ->
+                {
+                    assertEquals(-timestamp, checksum);
+                    receivedTimestamps.addLong(timestamp);
+                });
+
+            messageTransceiver.init(configuration);
+            try
+            {
+                int sent = 0;
+                long timestamp = 1_000;
+                while (sent < messages || receivedTimestamps.size() < messages)
+                {
+                    if (Thread.interrupted())
+                    {
+                        throw new IllegalStateException("run cancelled!");
+                    }
+
+                    if (sent < messages)
+                    {
+                        int sentBatch = 0;
+                        do
+                        {
+                            sentBatch +=
+                                messageTransceiver.send(burstSize - sentBatch, messageLength, timestamp, -timestamp);
+                            messageTransceiver.receive();
+                        }
+                        while (sentBatch < burstSize);
+
+                        for (int i = 0; i < burstSize; i++)
+                        {
+                            sentTimestamps.add(timestamp);
+                        }
+
+                        sent += burstSize;
+                        timestamp++;
+                    }
+
+                    if (receivedTimestamps.size() < messages)
+                    {
+                        messageTransceiver.receive();
+                    }
+
+                    if (null != error.get())
+                    {
+                        rethrowUnchecked(error.get());
+                    }
+                }
+            }
+            finally
+            {
+                messageTransceiver.destroy();
+            }
+        }
+        finally
+        {
+            running.set(false);
+            closeAll(clusteredServiceContainer, consensusModule, driver);
+        }
+
+        if (null != error.get())
+        {
+            rethrowUnchecked(error.get());
+        }
+
+        assertEquals(sentTimestamps, receivedTimestamps);
+    }
+
+    private static ErrorHandler errorHandler(final String context)
+    {
+        return (Throwable throwable) ->
+        {
+            System.err.println(context);
+            throwable.printStackTrace(System.err);
+            LangUtil.rethrowUnchecked(throwable);
+        };
+    }
+}
