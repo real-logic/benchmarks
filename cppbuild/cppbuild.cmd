896b5dda037f1de04fad174b0dd5cebabb89f7bf
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2022-02-23 13:15:35 +0100

    [Batch] Fail on invalid options.

--- cppbuild/cppbuild.cmd
+++ cppbuild/cppbuild.cmd
@@ -10,37 +10,29 @@ set "EXTRA_CMAKE_ARGS="
 :loop
 if not "%1"=="" (
     if "%1"=="--help" (
-        echo %0 [--c-warnings-as-errors] [--cxx-warnings-as-errors] [--debug-build] [--aeron-git-url $GIT_URL] [--aeron-git-tag $GIT_TAG]
+        echo %0 [--c-warnings-as-errors] [--cxx-warnings-as-errors] [--debug-build] [--aeron-git-url $GIT_URL] [--aeron-git-tag $GIT_TAG] [--aeron-git-sha $GIT_SHA] [--help]
         exit /b
-    )
-
-    if "%1"=="--c-warnings-as-errors" (
+    ) else if "%1"=="--c-warnings-as-errors" (
         set "EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DC_WARNINGS_AS_ERRORS=ON"
-    )
-
-    if "%1"=="--cxx-warnings-as-errors" (
+    ) else if "%1"=="--cxx-warnings-as-errors" (
         set "EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DCXX_WARNINGS_AS_ERRORS=ON"
-    )
-
-    if "%1"=="--debug-build" (
+    ) else if "%1"=="--debug-build" (
         set "EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DCMAKE_BUILD_TYPE=Debug"
         set "BUILD_DIR=%DIR%\Debug"
         set "BUILD_CONFIG=Debug"
-    )
-
-    if "%1"=="--aeron-git-url" (
+    ) else if "%1"=="--aeron-git-url" (
         set "EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DAERON_GIT_URL=%2"
         shift
-    )
-
-    if "%1"=="--aeron-git-tag" (
+    ) else if "%1"=="--aeron-git-tag" (
         set "EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DAERON_GIT_TAG=%2"
         shift
-    )
-
-    if "%1"=="--aeron-git-sha" (
+    ) else if "%1"=="--aeron-git-sha" (
         set "EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DAERON_GIT_SHA=%2"
         shift
+    ) else (
+        echo "Unknown option %%o"
+        echo "Use --help for help"
+        exit /b 1
     )
 
     shift
@@ -55,7 +47,6 @@ if EXIST %BUILD_DIR% rd /S /Q %BUILD_DIR%
 md %BUILD_DIR%
 pushd %BUILD_DIR%
 
-pushd %BUILD_DIR%
 cmake %EXTRA_CMAKE_ARGS% %SOURCE_DIR%
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
