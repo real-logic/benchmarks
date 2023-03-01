eebbfd2b571e29f18cee76d977d724650af7eead
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2023-03-01 11:38:10 +0100

    [Java] Enable gRPC TLS tests.

--- benchmarks-grpc/src/test/java/uk/co/real_logic/benchmarks/grpc/remote/GrpcTest.java
+++ benchmarks-grpc/src/test/java/uk/co/real_logic/benchmarks/grpc/remote/GrpcTest.java
@@ -19,6 +19,8 @@ package uk.co.real_logic.benchmarks.grpc.remote;
 import org.HdrHistogram.Histogram;
 import org.agrona.concurrent.NanoClock;
 import org.agrona.concurrent.SystemNanoClock;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import uk.co.real_logic.benchmarks.remote.Configuration;
@@ -28,37 +30,56 @@ import uk.co.real_logic.benchmarks.remote.SinglePersistedHistogram;
 
 import java.io.PrintStream;
 
+import static java.lang.System.clearProperty;
+import static java.lang.System.setProperty;
 import static org.mockito.Mockito.mock;
-import static uk.co.real_logic.benchmarks.grpc.remote.GrpcConfig.getServerBuilder;
+import static uk.co.real_logic.benchmarks.grpc.remote.GrpcConfig.*;
 
 class GrpcTest
 {
+    @BeforeEach
+    void before()
+    {
+        setProperty(CERTIFICATES_DIR_PROP_NAME, Configuration.tryResolveCertificatesDirectory().toString());
+    }
+
+    @AfterEach
+    void after()
+    {
+        clearProperty(SERVER_HOST_PROP_NAME);
+        clearProperty(SERVER_PORT_PROP_NAME);
+        clearProperty(TLS_PROP_NAME);
+        clearProperty(CERTIFICATES_DIR_PROP_NAME);
+    }
+
     @Test
     @Timeout(30)
     void messageLength32bytes() throws Exception
     {
-        test(10_000, 32, 10);
+        test(10_000, 32, 10, false);
     }
 
     @Test
     @Timeout(30)
     void messageLength1344bytes() throws Exception
     {
-        test(100, 1344, 1);
+        test(100, 1344, 1, true);
     }
 
     @Test
     @Timeout(30)
     void messageLength288bytes() throws Exception
     {
-        test(1000, 288, 5);
+        test(777, 288, 3, false);
     }
 
     private void test(
         final int numberOfMessages,
         final int messageLength,
-        final int burstSize) throws Exception
+        final int burstSize,
+        final boolean useTls) throws Exception
     {
+        setProperty(TLS_PROP_NAME, Boolean.toString(useTls));
         try (EchoServer server = new EchoServer(getServerBuilder()))
         {
             server.start();
