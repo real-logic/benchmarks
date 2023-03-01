b0ea6355a3e46687924413aed89f7950685b0e4e
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2023-03-01 15:56:00 +0100

    [Java] Add support for configuring client-side address for gRPC.

--- benchmarks-grpc/src/main/java/uk/co/real_logic/benchmarks/grpc/remote/GrpcConfig.java
+++ benchmarks-grpc/src/main/java/uk/co/real_logic/benchmarks/grpc/remote/GrpcConfig.java
@@ -15,6 +15,7 @@
  */
 package uk.co.real_logic.benchmarks.grpc.remote;
 
+import io.grpc.Attributes;
 import io.grpc.ManagedChannel;
 import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
 import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
@@ -25,6 +26,7 @@ import org.agrona.LangUtil;
 
 import javax.net.ssl.SSLException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -34,6 +36,8 @@ import static java.lang.System.getProperty;
 
 final class GrpcConfig
 {
+    public static final String CLIENT_HOST_PROP_NAME = "uk.co.real_logic.benchmarks.grpc.remote.client.host";
+    public static final String CLIENT_PORT_PROP_NAME = "uk.co.real_logic.benchmarks.grpc.remote.client.port";
     public static final String SERVER_HOST_PROP_NAME = "uk.co.real_logic.benchmarks.grpc.remote.server.host";
     public static final String SERVER_PORT_PROP_NAME = "uk.co.real_logic.benchmarks.grpc.remote.server.port";
     public static final String TLS_PROP_NAME = "uk.co.real_logic.benchmarks.grpc.remote.tls";
@@ -45,8 +49,16 @@ final class GrpcConfig
 
     public static ManagedChannel getServerChannel()
     {
-        final NettyChannelBuilder channelBuilder =
-            NettyChannelBuilder.forAddress(getServerHost(), getServerPort()).directExecutor();
+        final NettyChannelBuilder channelBuilder = NettyChannelBuilder
+            .forAddress(getServerHost(), getServerPort())
+            .localSocketPicker(new NettyChannelBuilder.LocalSocketPicker()
+            {
+                public SocketAddress createSocketAddress(final SocketAddress remoteAddress, final Attributes attrs)
+                {
+                    return new InetSocketAddress(getClientHost(), getClientPort());
+                }
+            })
+            .directExecutor();
         if (getBoolean(TLS_PROP_NAME))
         {
             final Path certificatesDir = certificatesDir();
@@ -99,13 +111,28 @@ final class GrpcConfig
 
     private static String getServerHost()
     {
-        final String host = getProperty(SERVER_HOST_PROP_NAME);
-        return null != host ? host : "127.0.0.1";
+        return getHost(SERVER_HOST_PROP_NAME);
     }
 
     private static int getServerPort()
     {
         return getInteger(SERVER_PORT_PROP_NAME, 13400);
+    }
+
+    private static String getClientHost()
+    {
+        return getHost(CLIENT_HOST_PROP_NAME);
+    }
+
+    private static int getClientPort()
+    {
+        return getInteger(CLIENT_PORT_PROP_NAME, 0);
+    }
+
+    private static String getHost(final String propName)
+    {
+        final String host = getProperty(propName);
+        return null != host ? host : "127.0.0.1";
     }
 
     private static Path certificatesDir()
