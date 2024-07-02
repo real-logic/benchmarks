058978e77dd819d20866a5e823d90271dd180c9d
James Masson <james.masson@weareadaptive.com>
2024-07-02 10:04:20 +0100

    K8s: Multi-interface and C media driver support

--- Dockerfile
+++ Dockerfile
@@ -1,36 +1,65 @@
-FROM gradle:8-jdk17-focal as builder
+FROM ubuntu:noble AS builder
+ARG AERON_VERSION_SELECTOR="--aeron-git-tag 1.44.1"
+ENV BENCHMARKS_PATH=/opt/aeron-benchmarks
+
+RUN apt-get update -y &&\
+  apt-get install -y \
+  openjdk-17-jdk-headless \
+  git \
+  cmake \
+  wget \
+  curl \
+  unzip \
+  build-essential &&\
+  wget -q https://services.gradle.org/distributions/gradle-8.8-bin.zip -O /tmp/gradle.zip &&\
+  unzip -d /opt /tmp/gradle.zip
+
+ENV PATH=${PATH};/opt/gradle-8.8/bin
+
 COPY . /tmp/benchmark-build
 WORKDIR /tmp/benchmark-build
+
+RUN ./cppbuild/cppbuild ${AERON_VERSION_SELECTOR} &&\
+  cd cppbuild/Release/aeron-prefix/src/aeron &&\
+  ./cppbuild/cppbuild --package --no-tests
 
 RUN --mount=type=cache,target=/root/.gradle \
   --mount=type=cache,target=/home/gradle/.gradle \
   --mount=type=cache,target=/tmp/benchmark-build/.gradle \
-  ./gradlew --no-daemon -i clean deployTar
+  export CDRIVER_PACKAGE="$(find cppbuild/Release/aeron-prefix/src/aeron/cppbuild/Release -maxdepth 1 -name 'aeron-*-Linux.tar.gz')" &&\
+  ./gradlew --no-daemon -i clean deployTar -Paeron.cdriver.package="${CDRIVER_PACKAGE}"
 
-FROM azul/zulu-openjdk:17-latest as runner
+RUN mkdir -p ${BENCHMARKS_PATH} &&\
+  tar -C ${BENCHMARKS_PATH} -xf /tmp/benchmark-build/build/distributions/benchmarks.tar
+
+FROM ubuntu:noble AS runner
 
 RUN apt-get update &&\
   apt-get install -y \
   tar \
+  openjdk-17-jdk-headless \
   gzip \
   iproute2 \
   bind9-utils \
   bind9-host \
+  sockperf \
+  irqbalance \
   jq \
   lsb-release \
   python3-pip \
   awscli \
   curl \
+  pipx \
   numactl \
   hwloc &&\
-  pip3 install --upgrade --user hdr-plot
+  pip install --break-system-packages hdr-plot
 
-ENV PATH /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/root/.local/bin
+ENV PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/root/.local/bin
+ENV BENCHMARKS_PATH=/opt/aeron-benchmarks
 
-COPY --from=builder /tmp/benchmark-build/build/distributions/benchmarks.tar /root/benchmarks.tar
-ENV BENCHMARKS_PATH /opt/aeron-benchmarks
-RUN mkdir -p ${BENCHMARKS_PATH} &&\
-  tar -C ${BENCHMARKS_PATH} -xf /root/benchmarks.tar &&\
-  rm -f /root/benchmarks.tar
+COPY --from=builder ${BENCHMARKS_PATH} ${BENCHMARKS_PATH}
+# The media driver packaging format does not match what the benchmarks expects
+# Manually fix this bit up.
+RUN ln -s ${BENCHMARKS_PATH}/bin/aeronmd ${BENCHMARKS_PATH}/scripts/aeron/aeronmd
 
 WORKDIR ${BENCHMARKS_PATH}/scripts
--- scripts/k8s-remote-testing.sh
+++ scripts/k8s-remote-testing.sh
@@ -9,7 +9,7 @@ function f_log() {
 
 function f_show_help() {
   f_log "Supported arguments are:"
-  echo "${0} (-n|--namespace) '<namespace>' (-t|--test) 'aeron-echo-dpdk|aeron-echo-java'"
+  echo "${0} (-n|--namespace '<namespace>' ) (-t|--test 'aeron-echo-dpdk|aeron-echo-java|aeron-echo-c' ) (-i|--interface <ignored for DPDK> 'eth0')"
 }
 
 while [[ $# -gt 0 ]]
@@ -23,12 +23,17 @@ do
       ;;
     -t|--test)
       TEST_TO_RUN="${2}"
-      if [[ "${TEST_TO_RUN}" == "aeron-echo-dpdk" || "${TEST_TO_RUN}" == "aeron-echo-java"  ]]
+      if [[ "${TEST_TO_RUN}" == "aeron-echo-dpdk" || "${TEST_TO_RUN}" == "aeron-echo-java" || "${TEST_TO_RUN}" == "aeron-echo-c" ]]
       then true
       else
-        f_log "Error: only supported tests are 'aeron-echo-dpdk' or 'aeron-echo-java' at the moment"
+        f_log "Error: only supported tests are 'aeron-echo-dpdk' or 'aeron-echo-java' or 'aeron-echo-c' at the moment"
         exit 1
       fi
+      shift
+      shift
+      ;;
+    -i|--interface)
+      INTERFACE="${2}"
       shift
       shift
       ;;
@@ -47,6 +52,7 @@ done
 # Standard vars
 K8S_NAMESPACE="${K8S_NAMESPACE:-default}"
 TEST_TO_RUN="${TEST_TO_RUN:-aeron-echo-java}"
+INTERFACE="${INTERFACE:-eth0}"
 
 TIMESTAMP="$(date +"%Y-%m-%d-%H-%M-%S")"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
@@ -79,8 +85,12 @@ then
 # Java Media Driver
 elif [[ "${TEST_TO_RUN}" =~ .*-java$ ]]
   then
-  AB0_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" get po  aeron-benchmark-0  -o json | jq -r ".status.podIP")"
-  AB1_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" get po  aeron-benchmark-1  -o json | jq -r ".status.podIP")"
+  AB0_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" exec aeron-benchmark-0  -c aeronmd-java -- bash -c "ip -4 -json addr show ${INTERFACE} |   jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
+  AB1_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" exec aeron-benchmark-1  -c aeronmd-java -- bash -c "ip -4 -json addr show ${INTERFACE} |   jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
+elif [[ "${TEST_TO_RUN}" =~ .*-c$ ]]
+  then
+  AB0_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" exec aeron-benchmark-0  -c aeronmd-c -- bash -c "ip -4 -json addr show ${INTERFACE} |   jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
+  AB1_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" exec aeron-benchmark-1  -c aeronmd-c -- bash -c "ip -4 -json addr show ${INTERFACE} |   jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
 else
   f_log "Media driver config not found"
   exit 1
